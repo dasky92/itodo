7916ac7eae2763d44684b8102fec2a2c36da3ee4
dasky92 <dasky92@163.com>
2025-12-27 21:44:53 +0800

    refactor: split theme region to single file

--- internal/tui/styles.go
+++ internal/tui/styles.go
@@ -2,25 +2,7 @@ package tui
 
 import "github.com/charmbracelet/lipgloss"
 
-type Theme struct {
-	Base      lipgloss.Color
-	Text      lipgloss.Color
-	Blue      lipgloss.Color
-	Green     lipgloss.Color
-	Pink      lipgloss.Color
-	Gray      lipgloss.Color
-	DarkGray  lipgloss.Color
-	Mauve     lipgloss.Color
-	Red       lipgloss.Color
-	Surface   lipgloss.Color
-	Highlight lipgloss.Color
-	Border    lipgloss.Color
-}
-
 var (
-	// Current Theme Palette
-	CurrentTheme Theme
-
 	// Styles
 	appStyle           lipgloss.Style
 	dateStyle          lipgloss.Style
@@ -47,80 +29,6 @@ var (
 	progressFullChar  = "█"
 	progressEmptyChar = "·"
 )
-
-// Define Themes
-var Themes = map[string]Theme{
-	"Catppuccin": {
-		Base:      lipgloss.Color("#1e1e2e"),
-		Text:      lipgloss.Color("#cdd6f4"),
-		Blue:      lipgloss.Color("#89b4fa"),
-		Green:     lipgloss.Color("#a6e3a1"),
-		Pink:      lipgloss.Color("#f5c2e7"),
-		Gray:      lipgloss.Color("#6c7086"),
-		DarkGray:  lipgloss.Color("#45475a"),
-		Mauve:     lipgloss.Color("#cba6f7"),
-		Red:       lipgloss.Color("#f38ba8"),
-		Surface:   lipgloss.Color("#313244"),
-		Highlight: lipgloss.Color("#313244"),
-		Border:    lipgloss.Color("#f5c2e7"),
-	},
-	"Monokai": {
-		Base:      lipgloss.Color("#272822"),
-		Text:      lipgloss.Color("#F8F8F2"),
-		Blue:      lipgloss.Color("#66D9EF"),
-		Green:     lipgloss.Color("#A6E22E"),
-		Pink:      lipgloss.Color("#F92672"), // Using Pink slot for Red/Pink
-		Gray:      lipgloss.Color("#75715E"),
-		DarkGray:  lipgloss.Color("#49483E"),
-		Mauve:     lipgloss.Color("#AE81FF"),
-		Red:       lipgloss.Color("#F92672"),
-		Surface:   lipgloss.Color("#3E3D32"),
-		Highlight: lipgloss.Color("#3E3D32"),
-		Border:    lipgloss.Color("#F92672"),
-	},
-	"OneDark": {
-		Base:      lipgloss.Color("#282c34"),
-		Text:      lipgloss.Color("#abb2bf"),
-		Blue:      lipgloss.Color("#61afef"),
-		Green:     lipgloss.Color("#98c379"),
-		Pink:      lipgloss.Color("#e06c75"),
-		Gray:      lipgloss.Color("#5c6370"),
-		DarkGray:  lipgloss.Color("#4b5263"),
-		Mauve:     lipgloss.Color("#c678dd"),
-		Red:       lipgloss.Color("#e06c75"),
-		Surface:   lipgloss.Color("#3e4452"),
-		Highlight: lipgloss.Color("#3e4452"),
-		Border:    lipgloss.Color("#61afef"),
-	},
-	"OneLight": {
-		Base:      lipgloss.Color("#fafafa"),
-		Text:      lipgloss.Color("#383a42"),
-		Blue:      lipgloss.Color("#4078f2"),
-		Green:     lipgloss.Color("#50a14f"),
-		Pink:      lipgloss.Color("#e45649"),
-		Gray:      lipgloss.Color("#a0a1a7"),
-		DarkGray:  lipgloss.Color("#d0d0d0"),
-		Mauve:     lipgloss.Color("#a626a4"),
-		Red:       lipgloss.Color("#e45649"),
-		Surface:   lipgloss.Color("#e5e5e6"),
-		Highlight: lipgloss.Color("#e5e5e6"),
-		Border:    lipgloss.Color("#4078f2"),
-	},
-	"Hacker": {
-		Base:      lipgloss.Color("#000000"),
-		Text:      lipgloss.Color("#00FF00"),
-		Blue:      lipgloss.Color("#008800"),
-		Green:     lipgloss.Color("#00FF00"),
-		Pink:      lipgloss.Color("#00AA00"),
-		Gray:      lipgloss.Color("#004400"),
-		DarkGray:  lipgloss.Color("#002200"),
-		Mauve:     lipgloss.Color("#00DD00"),
-		Red:       lipgloss.Color("#FF0000"), // Alert
-		Surface:   lipgloss.Color("#001100"),
-		Highlight: lipgloss.Color("#003300"),
-		Border:    lipgloss.Color("#00FF00"),
-	},
-}
 
 func InitStyles(theme Theme) {
 	CurrentTheme = theme
